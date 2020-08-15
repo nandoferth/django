24f92e9c6a8bcf0842e6da3cef47effeb119869d
nandoferth <vizcainolopezfernando1996@outlook.es>
2020-08-14 20:20:33 -0500

    rutas protegidas

--- Start_0/accounts/views.py
+++ Start_0/accounts/views.py
@@ -3,47 +3,54 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 from .forms import OrdersForm,OrdersProduct,CreationUserForm
 
 def loginPage(request):
-    if request.method == 'POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request,user)
+        if request.user.is_authenticated:
             return redirect('table')
         else:
-            messages.info(request,'file')
-    return render(request,'accounts/Login.html')
-
+            if request.method == 'POST':
+                username=request.POST.get('username')
+                password=request.POST.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request,user)
+                    return redirect('table')
+                else:
+                    messages.info(request,'file')
+            return render(request,'accounts/Login.html')
+@login_required(login_url='login')
 def logoutPage(request):
     logout(request)
     return redirect('login')
 def register(request):
-    form = CreationUserForm()
-    if request.method == 'POST':
-        form = CreationUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/login')
-    context={'form':form}
-    return render(request,'accounts/Register.html',context)
-
+    if request.user.is_authenticated:
+        return redirect('table')
+    else:
+        form = CreationUserForm()
+        if request.method == 'POST':
+            form = CreationUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/login')
+        context={'form':form}
+        return render(request,'accounts/Register.html',context)
+@login_required(login_url='login')
 def home(request):
     return HttpResponse("Home")
-
+@login_required(login_url='login')
 def Crud(request):
     return render(request,'accounts/Crud.html')
-
+@login_required(login_url='login')
 def CustomerPage(request, pk_test):
     user = Customer.objects.get(name=pk_test)
     product = user.orders_set.filter(status="Pending").count()
     context = {'user':user,'product':product}
     return render(request,'accounts/Customer.html',context)
-
+@login_required(login_url='login')
 def Table(request):
     
 #-----------------------------------------------------#
