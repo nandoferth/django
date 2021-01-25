5927c89e94ae3bf5349e0d278b6106f85970f6fb
nandoferth <vizcainolopezfernando1996@outlook.es>
2021-01-25 09:38:50 -0600

    Algunos ejemplos de moduloa

--- models/myapp/urls.py
+++ models/myapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.products),
   path('acounts/', views.acounts),
+  path('home/', views.acountCustomer),
 ]
--- models/myapp/views.py
+++ models/myapp/views.py
@@ -4,10 +4,18 @@ from .models import *
 # Create your views here.
 def products(request):
   products = product.objects.all()
-  return render(request, 'product.html', {'products':products})
+  productsSize = product.objects.filter(sizesProduct='S')
+  return render(request, 'product.html', {'products':products, 'productsSize':productsSize})
 
 def acounts(request):
   totalAcounts = acount.objects.count()
   acounts = acount.objects.all()
   context = {'totalAcounts':totalAcounts, 'acounts':acounts}
   return render(request, 'acount.html', context)
+
+def acountCustomer(request):
+  # user = customer.objects.first()
+  user = customer.objects.get(id=2)
+  typeUser = user.acount_set.all()
+  context = {'typeUser':typeUser}
+  return render(request, 'profle.html', context)
